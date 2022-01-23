9fcc43f8a13041062b94cdd333a6427b66aae2ed
Plumliil <476453574@qq.com>
2022-01-23 23:55:26 +0800

    lii-notice

---
+++ src/components/lii-notice/LiiNotice.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      标题:{{title}}
+      内容:{{message}}
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            title:'弹窗组件',
+            message:'组件内容'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
