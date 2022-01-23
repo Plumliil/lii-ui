f68303700bea2d77b3f244b6562ed94e496908d5
Plumliil <476453574@qq.com>
2022-01-23 23:52:16 +0800

    LiiButton

---
+++ src/components/LiiButton.vue
@@ -0,0 +1,40 @@
+<template>
+  <button
+    ref="buttonRef"
+    @click="$emit('click',[...args])"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "LiButton",
+  inheritAttrs:false,
+  created() {
+      
+  },
+  data() {
+    return {
+      buttonRef: "",
+      // size:this.size
+    };
+  },
+  mounted() {
+    if (this.$attrs.size === "little") {
+      console.log(this.$refs.buttonRef);
+    }
+    if(this.$attrs.color){
+        this.$refs.buttonRef.style.backgroundColor=this.$attrs.color;
+    }
+  },
+};
+</script>
+
+<style lang="less" scoped>
+    button{
+        width:70px;
+        height: 30px;
+        outline: none;
+    }
+</style>
