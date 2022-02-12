49074d5e1662d771b231b56e64d4f3a5cf64cad4
plumli <476453574@qq.com>
2022-02-12 22:53:21 +0800

    drag

--- src/components/lii-drag/LiiDrag.vue
+++ src/components/lii-drag/LiiDrag.vue
@@ -1,18 +1,50 @@
 <template>
   <h1>lii-drag</h1>
   <div>
-    <span>xx</span>
+    <span
+      v-for="(item, index) in nums"
+      :key="index"
+      @dragstart.stop="onDragstart"
+      @dragenter.stop="onDragenter"
+      @dragend.stop="onDragend"
+      draggable
+      >{{ item }}</span
+    >
   </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {};
+    return {
+      nums: [1, 2, 3, 4, 5, 6],
+    };
   },
-  methods: {},
+  methods: {
+      onDragstart(){
+          console.log('onDragstart');
+      },
+      onDragenter(){},
+      onDragend(){}
+  },
 };
 </script>
-
-<style>
+<style lang="less" scoped>
+div {
+  background-color: aquamarine;
+  height: 300px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  span {
+    background-color: cadetblue;
+    padding: 50px;
+    margin: 15px;
+    color: white;
+    font-size: 30px;
+    // transform: scale(1.2);
+    transition: 1s;
+    cursor: pointer;
+  }
+}
 </style>
