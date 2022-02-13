7cb9a0f6d42224613e6ee33f0b726f7dab3275c9
plumli <476453574@qq.com>
2022-02-13 21:37:48 +0800

    drag

---
+++ src/components/lii-drag/DragItem.vue
@@ -0,0 +1,46 @@
+<template>
+  <h1>lii-drag</h1>
+  <div
+    @dragstart.stop="onDragstart"
+    @dragenter.stop="onDragenter"
+    @dragend.stop="onDragend"
+    draggable
+    class="drag_item"
+  >
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      nums: [1, 2, 3, 4, 5, 6],
+    };
+  },
+  methods: {
+    onDragstart(index) {
+      console.log("onDragstart", index);
+      console.log(this.$el);
+    },
+    onDragenter(index) {
+      console.log("onDragenter", index);
+    },
+    onDragend(index) {
+      console.log("onDragend", index);
+    },
+  },
+};
+</script>
+<style lang="less" scoped>
+div {
+  background-color: cadetblue;
+  padding: 50px;
+  margin: 15px;
+  color: white;
+  font-size: 30px;
+  // transform: scale(1.2);
+  transition: 1s;
+  cursor: pointer;
+}
+</style>
--- src/components/lii-drag/LiiDrag.vue
+++ src/components/lii-drag/LiiDrag.vue
@@ -1,31 +1,51 @@
 <template>
-  <h1>lii-drag</h1>
-  <div>
-    <span
-      v-for="(item, index) in nums"
-      :key="index"
-      @dragstart.stop="onDragstart"
-      @dragenter.stop="onDragenter"
-      @dragend.stop="onDragend"
-      draggable
-      >{{ item }}</span
-    >
+  <div ref="liiDrag" @dragenter.prevent @dragover.prevent>
+    <!-- 阻止浏览器默认行为，不然会显示一个叉叉，不好看 -->
+    <slot />
+    xx
   </div>
 </template>
 
 <script>
+let Emitter = require("tiny-emitter");
+let emitter = new Emitter();
 export default {
+  name: "LiiDrag",
+  created() {
+    this.toDom = ""; // 拖拽时进入的元素
+    this.fromDom = ""; // 拖拽起始元素
+    this.children = []; // 存放所有子组件元素集合
+    emitter.on("dragstart", this.onDragstart); // 先注册后触发
+    emitter.on("dragenter", this.onDragenter); // 先注册后触发
+    emitter.on("dragend", this.onDragend); // 先注册后触发
+    emitter.on("putChild", (child) => {
+      this.children.push(child);
+    }); // 先注册后触发
+  },
   data() {
     return {
       nums: [1, 2, 3, 4, 5, 6],
     };
   },
   methods: {
-      onDragstart(){
-          console.log('onDragstart');
-      },
-      onDragenter(){},
-      onDragend(){}
+    onDragstart(el) {
+      this.fromDom = el;
+      // console.log("onDragstart", index);
+      // console.log(this.$el);
+    },
+    onDragenter(el) {
+      this.toDom=el; // 拖拽会不停出发enter 所以要记录进入哪个元素
+      if(this.fromDom===this.toDom) return;
+      this.isPrevNode()
+    },
+    onDragend() {
+      // console.log("onDragend", index);
+    },
+    // 判断to是否在from前
+    isPrevNode(from,to){
+      console.log(from);
+      console.log(to);
+    }
   },
 };
 </script>
@@ -37,6 +57,7 @@ div {
   justify-content: center;
   align-items: center;
   span {
+    display: inline-block;
     background-color: cadetblue;
     padding: 50px;
     margin: 15px;
