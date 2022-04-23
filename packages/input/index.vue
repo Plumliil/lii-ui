72cab5ee6d71857d001a87d31e6c8aa5bf4b0926
plumliil <476453574@qq.com>
2022-04-23 12:15:13 +0800

    input update

--- packages/input/index.vue
+++ packages/input/index.vue
@@ -22,15 +22,21 @@ const props = defineProps({
   modelValue: String | Number,
   placeholder: {
     type: String,
-    default: "请输入内容",
+    default: "请输入...",
+  },
+  size: {
+    type: String,
+    default: "mid",
   },
 });
 const emits = defineEmits(["update:modelValue"]);
 const liiInputClass = computed(() => {
-  return {
+  let iptClass = {
     "lii-input": true,
-    "lii-input--disabled":props.disabled
+    "lii-input--disabled": props.disabled,
+    ["lii-" + props.size + "-input"]: true,
   };
+  return iptClass;
 });
 const input = (e) => {
   emits("update:modelValue", e.target.value);
@@ -52,6 +58,7 @@ const input = (e) => {
   padding: 0 15px;
   transition: border-color 0.3s cubic-bezier(0.645, 0.045, 0.355, 1);
   width: 250px;
+  margin: 3px;
   cursor: pointer;
   &:hover {
     border-color: @info;
@@ -63,8 +70,28 @@ const input = (e) => {
     font-size: 14px;
     color: #aeafb3;
   }
+  &.lii-big-input {
+    width: 300px;
+    height: 48px;
+    padding: 0 18px;
+    &::placeholder {
+      font-size: 16.8px;
+    }
+  }
+  &.lii-mid-input {
+    transform: scale(1);
+  }
+  &.lii-small-input {
+    width: 175px;
+    height: 28px;
+    line-height: 20px;
+    padding: 0 10.5px;
+    &::placeholder {
+      font-size: 9.8px;
+    }
+  }
 }
-.lii-input--disabled{
+.lii-input--disabled {
   &:hover {
     border-color: #dcdfe6;
   }
