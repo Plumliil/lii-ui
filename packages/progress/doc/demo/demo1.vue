886b5adc622dad9718afbe9523409bf41918c6ab
plumliil <plumliil@foxmail.com>
2022-07-26 17:49:31 +0800

    progress 进度条

--- packages/progress/doc/demo/demo1.vue
+++ packages/progress/doc/demo/demo1.vue
@@ -1,6 +1,6 @@
 <template>
-    <lii-progress :textInside="true" :percentage="100"></lii-progress>
-    <lii-progress :textInside="true" :percentage="80" status="primary"></lii-progress>
+    <lii-progress :textInside="true" :percentage="100" color="red"></lii-progress>
+    <lii-progress :width="200" :textInside="true" :percentage="80" status="primary"></lii-progress>
     <lii-progress :textInside="true" :percentage="60" status="success"></lii-progress>
     <lii-progress :textInside="true" :percentage="40" status="warning"></lii-progress>
     <lii-progress :textInside="true" :percentage="20" status="error"></lii-progress>
