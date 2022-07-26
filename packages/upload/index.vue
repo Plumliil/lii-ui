853eec71166d4e2c8c5e216ab061a44360d9bfa9
plumliil <plumliil@foxmail.com>
2022-07-26 17:51:25 +0800

    upload 提交

--- packages/upload/index.vue
+++ packages/upload/index.vue
@@ -6,8 +6,10 @@
       <li v-for="(item,index) in fileList" :key="index">
         <span>{{item.name}}</span>
         <span class="delete-span" @click="deleteFile(index)">x</span>
+
       </li>
     </div>
+    <div class="area-box" @click="fileChange" ref="areaRef" :class="{activeArea:isActive}"></div>
   </div>
 </template>
 
@@ -22,6 +24,9 @@
     ref
   } from '@vue/reactivity';
   import liiButton from '../button/index.vue';
+  import {
+    onMounted
+  } from '@vue/runtime-core';
   const props = defineProps({
     multiple: {
       type: Boolean,
@@ -39,6 +44,8 @@
   const emits = defineEmits(['change', 'update:fileList']);
   const fileList = reactive([]);
   const inpRef = ref(null);
+  const areaRef = ref(null);
+  const isActive = ref(false);
   const fileChange = () => {
     inpRef.value.click()
   }
@@ -46,13 +53,42 @@
     fileList.unshift(...e.target.files)
     emits('change', fileList)
   }
-  const deleteFile=(index)=>{
-    fileList.splice(index,1);
-    emits('update:fileList',fileList)
+  const deleteFile = (index) => {
+    fileList.splice(index, 1);
+    emits('3333333333', fileList)
   }
+  onMounted(() => {
+    console.log(areaRef.value);
+    let dropArea = areaRef.value;
+    dropArea.addEventListener('drop', (e) => {
+      e.stopPropagation();
+      e.preventDefault();
+      fileList.unshift(...e.dataTransfer.files)
+      console.log(e.dataTransfer.files);
+      isActive.value = false;
+    }, false)
+    dropArea.addEventListener('dragleave', (e) => {
+      e.stopPropagation();
+      e.preventDefault();
+      console.log('经过');
+      isActive.value = false;
+    }, false)
+    dropArea.addEventListener('dragenter', (e) => {
+      e.stopPropagation();
+      e.preventDefault();
+      console.log('进入');
+      isActive.value = true;
+    }, false)
+    dropArea.addEventListener('dragover', (e) => {
+      e.stopPropagation();
+      e.preventDefault();
+      console.log('完成');
+      isActive.value = true;
+    }, false)
+  })
 </script>
 
-<style lang='less' scoped>
+<style lang='less'>
   .lii-upload {
     .file-input {
       display: none;
@@ -64,10 +100,23 @@
         display: flex;
         align-items: center;
         justify-content: space-between;
-        .delete-span{
+
+        .delete-span {
           cursor: pointer;
         }
-}
+      }
+    }
+
+    .area-box {
+      width: 200px;
+      height: 200px;
+      border: @baseBorder;
+      margin: 10px 0;
+      transition: .5s;
+    }
+
+    .activeArea {
+      border: 1px solid @primary;
     }
   }
-</style>
+</style>.o8
