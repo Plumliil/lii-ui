c79b9c239ecca869db5821ecc54c674e2915f6f1
plumliil <plumliil@foxmail.com>
2022-08-05 16:31:56 +0800

    select

--- docs/.vuepress/.temp/pages/reference/components/select.html.vue
+++ docs/.vuepress/.temp/pages/reference/components/select.html.vue
@@ -1,2 +1,201 @@
 <template><div><h1 id="select-下拉框" tabindex="-1"><a class="header-anchor" href="#select-下拉框" aria-hidden="true">#</a> Select 下拉框</h1>
+<h2 id="基本使用" tabindex="-1"><a class="header-anchor" href="#基本使用" aria-hidden="true">#</a> 基本使用</h2>
+<lii-select :options="options"></lii-select>
+<div class="language-vue ext-vue line-numbers-mode"><pre v-pre class="language-vue"><code><span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>template</span><span class="token punctuation">></span></span>
+  <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>lii-select</span> <span class="token attr-name">:options</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">"</span>options<span class="token punctuation">"</span></span><span class="token punctuation">></span></span><span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>lii-select</span><span class="token punctuation">></span></span>
+<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>template</span><span class="token punctuation">></span></span>
+
+<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>script</span> <span class="token attr-name">setup</span><span class="token punctuation">></span></span><span class="token script"><span class="token language-javascript">
+<span class="token keyword">import</span> <span class="token punctuation">{</span> reactive <span class="token punctuation">}</span> <span class="token keyword">from</span> <span class="token string">"vue"</span><span class="token punctuation">;</span>
+<span class="token keyword">const</span> state <span class="token operator">=</span> <span class="token function">reactive</span><span class="token punctuation">(</span><span class="token punctuation">{</span>
+  <span class="token literal-property property">options</span><span class="token operator">:</span> <span class="token punctuation">[</span>
+    <span class="token punctuation">{</span>
+      <span class="token literal-property property">label</span><span class="token operator">:</span> <span class="token string">"选项一"</span><span class="token punctuation">,</span>
+      <span class="token literal-property property">value</span><span class="token operator">:</span> <span class="token string">"0"</span><span class="token punctuation">,</span>
+    <span class="token punctuation">}</span><span class="token punctuation">,</span>
+    <span class="token punctuation">{</span>
+      <span class="token literal-property property">label</span><span class="token operator">:</span> <span class="token string">"选项二"</span><span class="token punctuation">,</span>
+      <span class="token literal-property property">value</span><span class="token operator">:</span> <span class="token string">"1"</span><span class="token punctuation">,</span>
+    <span class="token punctuation">}</span><span class="token punctuation">,</span>
+    <span class="token punctuation">{</span>
+      <span class="token literal-property property">label</span><span class="token operator">:</span> <span class="token string">"选项三"</span><span class="token punctuation">,</span>
+      <span class="token literal-property property">value</span><span class="token operator">:</span> <span class="token string">"2"</span><span class="token punctuation">,</span>
+    <span class="token punctuation">}</span>
+  <span class="token punctuation">]</span><span class="token punctuation">,</span>
+<span class="token punctuation">}</span><span class="token punctuation">)</span><span class="token punctuation">;</span>
+<span class="token keyword">const</span> <span class="token punctuation">{</span> options <span class="token punctuation">}</span> <span class="token operator">=</span> state<span class="token punctuation">;</span>
+</span></span><span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>script</span><span class="token punctuation">></span></span>
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="禁用状态" tabindex="-1"><a class="header-anchor" href="#禁用状态" aria-hidden="true">#</a> 禁用状态</h2>
+<p><lii-select
+    :options="options1"
+    filedLabel="name"
+    filedValue="id"
+    @change="getSelect"></lii-select></p>
+<div class="language-vue ext-vue line-numbers-mode"><pre v-pre class="language-vue"><code><span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>template</span><span class="token punctuation">></span></span>
+  <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>lii-select</span>
+    <span class="token attr-name">:options</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">"</span>options<span class="token punctuation">"</span></span>
+    <span class="token attr-name">filedLabel</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">"</span>name<span class="token punctuation">"</span></span>
+    <span class="token attr-name">filedValue</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">"</span>id<span class="token punctuation">"</span></span>
+  <span class="token punctuation">></span></span><span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>lii-select</span><span class="token punctuation">></span></span>
+<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>template</span><span class="token punctuation">></span></span>
+
+<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>script</span> <span class="token attr-name">setup</span><span class="token punctuation">></span></span><span class="token script"><span class="token language-javascript">
+<span class="token keyword">import</span> <span class="token punctuation">{</span> reactive <span class="token punctuation">}</span> <span class="token keyword">from</span> <span class="token string">"vue"</span><span class="token punctuation">;</span>
+<span class="token keyword">const</span> state <span class="token operator">=</span> <span class="token function">reactive</span><span class="token punctuation">(</span><span class="token punctuation">{</span>
+  <span class="token literal-property property">options</span><span class="token operator">:</span> <span class="token punctuation">[</span>
+    <span class="token punctuation">{</span>
+      <span class="token literal-property property">name</span><span class="token operator">:</span> <span class="token string">"选项一"</span><span class="token punctuation">,</span>
+      <span class="token literal-property property">id</span><span class="token operator">:</span> <span class="token string">"0"</span><span class="token punctuation">,</span>
+    <span class="token punctuation">}</span><span class="token punctuation">,</span>
+    <span class="token punctuation">{</span>
+      <span class="token literal-property property">name</span><span class="token operator">:</span> <span class="token string">"选项二"</span><span class="token punctuation">,</span>
+      <span class="token literal-property property">id</span><span class="token operator">:</span> <span class="token string">"1"</span><span class="token punctuation">,</span>
+    <span class="token punctuation">}</span><span class="token punctuation">,</span>
+    <span class="token punctuation">{</span>
+      <span class="token literal-property property">name</span><span class="token operator">:</span> <span class="token string">"选项三"</span><span class="token punctuation">,</span>
+      <span class="token literal-property property">id</span><span class="token operator">:</span> <span class="token string">"2"</span><span class="token punctuation">,</span>
+    <span class="token punctuation">}</span>
+  <span class="token punctuation">]</span><span class="token punctuation">,</span>
+<span class="token punctuation">}</span><span class="token punctuation">)</span><span class="token punctuation">;</span>
+<span class="token keyword">const</span> <span class="token punctuation">{</span> options <span class="token punctuation">}</span> <span class="token operator">=</span> state<span class="token punctuation">;</span>
+</span></span><span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>script</span><span class="token punctuation">></span></span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="默认选中" tabindex="-1"><a class="header-anchor" href="#默认选中" aria-hidden="true">#</a> 默认选中</h2>
+<p><lii-select
+    :options="options"
+    v-model="selVal">
+</lii-select></p>
+<div class="language-vue ext-vue line-numbers-mode"><pre v-pre class="language-vue"><code><span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>template</span><span class="token punctuation">></span></span>
+  <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>lii-select</span>
+    <span class="token attr-name">:options</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">"</span>options<span class="token punctuation">"</span></span>
+    <span class="token attr-name">filedLabel</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">"</span>name<span class="token punctuation">"</span></span>
+    <span class="token attr-name">filedValue</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">"</span>id<span class="token punctuation">"</span></span>
+    <span class="token attr-name">@change</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">"</span>getSelect<span class="token punctuation">"</span></span>
+  <span class="token punctuation">></span></span><span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>lii-select</span><span class="token punctuation">></span></span>
+<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>template</span><span class="token punctuation">></span></span>
+
+<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>script</span> <span class="token attr-name">setup</span><span class="token punctuation">></span></span><span class="token script"><span class="token language-javascript">
+<span class="token keyword">import</span> <span class="token punctuation">{</span> reactive<span class="token punctuation">,</span> ref <span class="token punctuation">}</span> <span class="token keyword">from</span> <span class="token string">"vue"</span><span class="token punctuation">;</span>
+<span class="token keyword">const</span> selVal<span class="token operator">=</span><span class="token function">ref</span><span class="token punctuation">(</span><span class="token string">'0'</span><span class="token punctuation">)</span><span class="token punctuation">;</span>
+<span class="token keyword">const</span> state <span class="token operator">=</span> <span class="token function">reactive</span><span class="token punctuation">(</span><span class="token punctuation">{</span>
+  <span class="token literal-property property">options</span><span class="token operator">:</span> <span class="token punctuation">[</span>
+    <span class="token punctuation">{</span>
+      <span class="token literal-property property">name</span><span class="token operator">:</span> <span class="token string">"选项一"</span><span class="token punctuation">,</span>
+      <span class="token literal-property property">id</span><span class="token operator">:</span> <span class="token string">"0"</span><span class="token punctuation">,</span>
+    <span class="token punctuation">}</span><span class="token punctuation">,</span>
+    <span class="token punctuation">{</span>
+      <span class="token literal-property property">name</span><span class="token operator">:</span> <span class="token string">"选项二"</span><span class="token punctuation">,</span>
+      <span class="token literal-property property">id</span><span class="token operator">:</span> <span class="token string">"1"</span><span class="token punctuation">,</span>
+    <span class="token punctuation">}</span><span class="token punctuation">,</span>
+    <span class="token punctuation">{</span>
+      <span class="token literal-property property">name</span><span class="token operator">:</span> <span class="token string">"选项三"</span><span class="token punctuation">,</span>
+      <span class="token literal-property property">id</span><span class="token operator">:</span> <span class="token string">"2"</span><span class="token punctuation">,</span>
+    <span class="token punctuation">}</span>
+  <span class="token punctuation">]</span><span class="token punctuation">,</span>
+<span class="token punctuation">}</span><span class="token punctuation">)</span><span class="token punctuation">;</span>
+<span class="token keyword">const</span> <span class="token punctuation">{</span> options <span class="token punctuation">}</span> <span class="token operator">=</span> state<span class="token punctuation">;</span>
+<span class="token keyword">const</span> <span class="token function-variable function">getSelect</span> <span class="token operator">=</span> <span class="token punctuation">(</span><span class="token parameter">v</span><span class="token punctuation">)</span> <span class="token operator">=></span> <span class="token punctuation">{</span>
+  console<span class="token punctuation">.</span><span class="token function">log</span><span class="token punctuation">(</span>v<span class="token punctuation">)</span><span class="token punctuation">;</span>
+<span class="token punctuation">}</span><span class="token punctuation">;</span>
+</span></span><span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>script</span><span class="token punctuation">></span></span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="属性" tabindex="-1"><a class="header-anchor" href="#属性" aria-hidden="true">#</a> 属性</h2>
+<table style="text-align:center">
+    <tr>
+        <td>参数</td>
+        <td>说明</td>
+        <td>类型</td>
+        <td>可选值</td>
+        <td>默认值</td>
+    </tr>
+    <tr>
+        <td>options</td>
+        <td>渲染选项数据</td>
+        <td>Boolean</td>
+        <td>——</td>
+        <td>true</td>
+    </tr>
+    <tr>
+        <td>disabled</td>
+        <td>选框是否可用</td>
+        <td>Boolean</td>
+        <td>——</td>
+        <td>true</td>
+    </tr>
+    <tr>
+        <td>filedLabel</td>
+        <td>选择选项名</td>
+        <td>String</td>
+        <td>——</td>
+        <td>label</td>
+    </tr>
+    <tr>
+        <td>filedValue</td>
+        <td>选择选项值</td>
+        <td>String</td>
+        <td>——</td>
+        <td>value</td>
+    </tr>
+    <tr>
+        <td>placeholder</td>
+        <td>选框默认值</td>
+        <td>String</td>
+        <td>——</td>
+        <td>请选择</td>
+    </tr>
+</table>
+<h2 id="事件" tabindex="-1"><a class="header-anchor" href="#事件" aria-hidden="true">#</a> 事件</h2>
+<table style="text-align:center">
+    <tr>
+        <td>事件名称</td>
+        <td>说明</td>
+        <td>回调参数</td>
+        <td>示例</td>
+    </tr>
+    <tr>
+        <td>change</td>
+        <td>选项发生变化时触发</td>
+        <td>(event: Value)</td>
+        <td>change=(value)=>{console.log(value)}</td>
+    </tr>
+</table>
 </div></template>
+
+<script setup>
+import { reactive, ref } from "vue";
+const selVal=ref('0');
+const state = reactive({
+  options: [
+    {
+      label: "选项一",
+      value: "0",
+    },
+    {
+      label: "选项二",
+      value: "1",
+    },
+    {
+      label: "选项三",
+      value: "2",
+    }
+  ],
+  options1: [
+    {
+      name: "选项一",
+      id: "0",
+    },
+    {
+      name: "选项二",
+      id: "1",
+      disabled: true,
+    },
+    {
+      name: "选项三",
+      id: "2",
+    }
+  ],
+});
+const { options,options1 } = state;
+const getSelect = (v) => {
+  console.log(v);
+};
+</script>
--- docs/reference/components/select.md
+++ docs/reference/components/select.md
@@ -1,1 +1,232 @@
 # Select 下拉框
+
+## 基本使用
+<lii-select :options="options"></lii-select>
+
+
+```vue
+<template>
+  <lii-select :options="options"></lii-select>
+</template>
+
+<script setup>
+import { reactive } from "vue";
+const state = reactive({
+  options: [
+    {
+      label: "选项一",
+      value: "0",
+    },
+    {
+      label: "选项二",
+      value: "1",
+    },
+    {
+      label: "选项三",
+      value: "2",
+    }
+  ],
+});
+const { options } = state;
+</script>
+
+```
+
+## 禁用状态
+
+<lii-select
+    :options="options1"
+    filedLabel="name"
+    filedValue="id"
+    @change="getSelect"></lii-select>
+
+```vue
+<template>
+  <lii-select
+    :options="options"
+    filedLabel="name"
+    filedValue="id"
+  ></lii-select>
+</template>
+
+<script setup>
+import { reactive } from "vue";
+const state = reactive({
+  options: [
+    {
+      name: "选项一",
+      id: "0",
+    },
+    {
+      name: "选项二",
+      id: "1",
+    },
+    {
+      name: "选项三",
+      id: "2",
+    }
+  ],
+});
+const { options } = state;
+</script>
+```
+
+
+## 默认选中
+
+<lii-select
+    :options="options"
+    v-model="selVal">
+    </lii-select>
+
+```vue
+<template>
+  <lii-select
+    :options="options"
+    filedLabel="name"
+    filedValue="id"
+    @change="getSelect"
+  ></lii-select>
+</template>
+
+<script setup>
+import { reactive, ref } from "vue";
+const selVal=ref('0');
+const state = reactive({
+  options: [
+    {
+      name: "选项一",
+      id: "0",
+    },
+    {
+      name: "选项二",
+      id: "1",
+    },
+    {
+      name: "选项三",
+      id: "2",
+    }
+  ],
+});
+const { options } = state;
+const getSelect = (v) => {
+  console.log(v);
+};
+</script>
+```
+
+
+
+
+
+
+
+## 属性
+<table style="text-align:center">
+    <tr>
+        <td>参数</td>
+        <td>说明</td>
+        <td>类型</td>
+        <td>可选值</td>
+        <td>默认值</td>
+    </tr>
+    <tr>
+        <td>options</td>
+        <td>渲染选项数据</td>
+        <td>Boolean</td>
+        <td>——</td>
+        <td>true</td>
+    </tr>
+    <tr>
+        <td>disabled</td>
+        <td>选框是否可用</td>
+        <td>Boolean</td>
+        <td>——</td>
+        <td>true</td>
+    </tr>
+    <tr>
+        <td>filedLabel</td>
+        <td>选择选项名</td>
+        <td>String</td>
+        <td>——</td>
+        <td>label</td>
+    </tr>
+    <tr>
+        <td>filedValue</td>
+        <td>选择选项值</td>
+        <td>String</td>
+        <td>——</td>
+        <td>value</td>
+    </tr>
+    <tr>
+        <td>placeholder</td>
+        <td>选框默认值</td>
+        <td>String</td>
+        <td>——</td>
+        <td>请选择</td>
+    </tr>
+</table>
+
+## 事件
+<table style="text-align:center">
+    <tr>
+        <td>事件名称</td>
+        <td>说明</td>
+        <td>回调参数</td>
+        <td>示例</td>
+    </tr>
+    <tr>
+        <td>change</td>
+        <td>选项发生变化时触发</td>
+        <td>(event: Value)</td>
+        <td>change=(value)=>{console.log(value)}</td>
+    </tr>
+</table>
+
+
+
+
+
+
+
+
+
+<script setup>
+import { reactive, ref } from "vue";
+const selVal=ref('0');
+const state = reactive({
+  options: [
+    {
+      label: "选项一",
+      value: "0",
+    },
+    {
+      label: "选项二",
+      value: "1",
+    },
+    {
+      label: "选项三",
+      value: "2",
+    }
+  ],
+  options1: [
+    {
+      name: "选项一",
+      id: "0",
+    },
+    {
+      name: "选项二",
+      id: "1",
+      disabled: true,
+    },
+    {
+      name: "选项三",
+      id: "2",
+    }
+  ],
+});
+const { options,options1 } = state;
+const getSelect = (v) => {
+  console.log(v);
+};
+</script>
