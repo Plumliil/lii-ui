b1e06988c2419c67bef6ae7ee62a7e4cea4d57b1
plumliil <plumliil@foxmail.com>
2022-08-05 16:30:50 +0800

    init

--- docs/.vuepress/.temp/pages/guide/init.html.vue
+++ docs/.vuepress/.temp/pages/guide/init.html.vue
@@ -1,2 +1,5 @@
-<template><div><h1 id="安装" tabindex="-1"><a class="header-anchor" href="#安装" aria-hidden="true">#</a> 安装</h1>
-</div></template>
+<template><div><h1 id="安装使用" tabindex="-1"><a class="header-anchor" href="#安装使用" aria-hidden="true">#</a> 安装使用</h1>
+<h2 id="npm-安装" tabindex="-1"><a class="header-anchor" href="#npm-安装" aria-hidden="true">#</a> npm 安装</h2>
+<p>推荐使用 npm 的方式安装，它能更好地和 webpack 打包工具配合使用。</p>
+<div class="language-bash ext-sh line-numbers-mode"><pre v-pre class="language-bash"><code><span class="token function">npm</span> i element-ui -S
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div></div></template>
